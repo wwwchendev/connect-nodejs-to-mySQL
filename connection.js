cab42509c83319c61a6e4e74a8eb27049b3ae37d
wwwchendev <wwwchendev@gmail.com>
2024-02-01 12:03:56 +0800

    連線至mySQL資料庫

---
+++ connection.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+// console.log('DB_HOST:', process.env.DB_HOST); 測試環境變數讀取
+
+const mysql = require('mysql2/promise');
+
+async function connectToMySQL() {
+  const connection = await mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  });
+  console.log('連線成功');
+  // 關閉連線
+  connection.end();
+}
+connectToMySQL();
+
